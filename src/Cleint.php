650042e1c0ec062a50a174d2fe8b5135da240059
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-28 17:27:48 -0700

    add setup for stylist and client tests

---
+++ src/Cleint.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Stylist.php";
+    require_once "src/Client.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+
+    $DB = new PDO($server, $username, $password);
+    class ClientTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Client::deleteAll();
+            Stylist::deleteAll();
+        }
