564aadded8bee8cb1f1ebe7179f62a91fb318d43
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-28 17:31:54 -0700

    rename files

---
+++ tests/StylistTest.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Stylist.php";
+    require_once "src/Client.php";
+
+
+    $server = 'mysql:host=localhost;=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        }
+            Stylist::deleteAll();
+        }
+
+
+
+?>
