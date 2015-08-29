c959d2401c2e376cb95914ee032b1add9791ea7f
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-28 18:02:44 -0700

    add class in src file and assign properties + construct

---
+++ src/Client.php
@@ -0,0 +1,12 @@
+<?php
+    class Client
+    {
+        private $client_name;
+        private $id;
+        private $stylist_id;
+        function __construct($client_name, $id = null, $stylist_id)
+        {
+            $this->client_name = $client_name;
+            $this->id = $id;
+            $this->stylist_id = $stylist_id;
+        }
